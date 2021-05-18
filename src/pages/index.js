ede25cf0191c50804bd993cfc440ef49dd8d2a3c
Gianluca Belvisi <popippo@gmail.com>
2021-05-18 23:32:12 +0200

    removed imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,6 @@
 import React from "react"
 
-import Layout from "../components/Layout"
 import Seo from "../components/Seo"
-import Hero from "../components/Hero";
-import Trips from "../components/Trips";
-import Testimonials from "../components/Testimonials";
-import Stats from "../components/Stats";
-import Email from "../components/Email";
 
 const Index = () => (
         <Seo title="Home"/>
