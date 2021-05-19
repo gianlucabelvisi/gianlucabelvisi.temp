0774981186b51a96a0f0f2a97258b17d1a85ef5d
Gianluca Belvisi <popippo@gmail.com>
2021-05-19 06:46:26 +0200

    Added testimonials

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 
 import Seo from "../components/Seo"
 import Layout from "../components/Layout";
+import Testimonials from "../components/Testimonials";
 
 const Index = () => (
     <Layout>
         <Seo title="Home"/>
+        <Testimonials/>
     </Layout>
 )
 
