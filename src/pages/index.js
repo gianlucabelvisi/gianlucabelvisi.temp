9f776de927fcc11aa628613284ce319dfdb49e30
Gianluca Belvisi <popippo@gmail.com>
2021-05-19 06:52:08 +0200

    Added Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import React from "react"
 import Seo from "../components/Seo"
 import Layout from "../components/Layout";
 import Testimonials from "../components/Testimonials";
+import Hero from "../components/Hero";
 
 const Index = () => (
     <Layout>
         <Seo title="Home"/>
+        <Hero/>
         <Testimonials/>
     </Layout>
 )
