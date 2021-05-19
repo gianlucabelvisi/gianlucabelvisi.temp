4bbca006bf5b90692cf55d8d1377d647d0f593b3
Gianluca Belvisi <popippo@gmail.com>
2021-05-19 06:58:16 +0200

    Added trips

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,14 @@ import Seo from "../components/Seo"
 import Layout from "../components/Layout";
 import Testimonials from "../components/Testimonials";
 import Hero from "../components/Hero";
+import Trips from "../components/Trips";
+import Stats from "../components/Stats";
 
 const Index = () => (
     <Layout>
         <Seo title="Home"/>
         <Hero/>
+        <Trips heading="My favorite stuff"/>
         <Testimonials/>
     </Layout>
 )
