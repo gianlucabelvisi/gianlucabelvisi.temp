fc454dc526e2e699f6a5499e85b3fac079e8cb15
Gianluca Belvisi <popippo@gmail.com>
2021-05-19 07:20:23 +0200

    Brought back trips but without images

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,14 @@ import Testimonials from "../components/Testimonials";
 import Hero from "../components/Hero";
 import Stats from "../components/Stats";
 import Email from "../components/Email";
+import Trips from "../components/Trips";
 
 const Index = () => (
     <Layout>
         <Seo title="Home"/>
         <Hero/>
         <Testimonials/>
+        <Trips heading="My favorite stuff"/>
         <Stats/>
         <Email/>
     </Layout>
