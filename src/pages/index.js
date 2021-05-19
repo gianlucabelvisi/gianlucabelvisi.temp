6d76116758aa914fb2974521238f80ed0b2c8d09
Gianluca Belvisi <popippo@gmail.com>
2021-05-19 07:06:41 +0200

    Reverted trips

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Seo from "../components/Seo"
 import Layout from "../components/Layout";
 import Testimonials from "../components/Testimonials";
 import Hero from "../components/Hero";
-import Trips from "../components/Trips";
 import Stats from "../components/Stats";
 import Email from "../components/Email";
 
@@ -12,7 +11,6 @@ const Index = () => (
     <Layout>
         <Seo title="Home"/>
         <Hero/>
-        <Trips heading="My favorite stuff"/>
         <Testimonials/>
         <Stats/>
         <Email/>
